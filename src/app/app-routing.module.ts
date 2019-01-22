8d299c04475fa8cda8a06ae2f75b1c8d351d527a
Sajben Zsolt <zsoltsajben@users.noreply.github.com>
2019-01-22 21:32:42 +0100

    lazy loading, preloading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { CoreModule } from './core/core.module';
-import { LinksModule } from './links/links.module';
+// import { LinksModule } from './links/links.module';
 
 const routes: Routes = [
   { path: '', loadChildren: () => CoreModule },
-  { path: 'links', loadChildren: () => LinksModule }
+  { path: 'links', loadChildren: './links/links.module#LinksModule' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
